9615e46f013e6ce41e48ff4aba9b78f2f69f43f6
AntonZinovik <44465549+AntonZinovik@users.noreply.github.com>
2023-11-20 13:12:55 +0300

    Initial commit

---
+++ src/template-net7.0/template-net7.0/Program.cs
@@ -0,0 +1,48 @@
+using System.Reflection;
+
+using Microsoft.OpenApi.Models;
+
+using Serilog;
+
+using template_net7._0.Middlewares;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Template project",
+        Description = "Template project"
+    });
+
+    var xmlFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFileName));
+});
+
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom
+                                                                 .Configuration(context.Configuration));
+
+var app = builder.Build();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options => { options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1"); });
+}
+
+app.UseSerilogRequestLogging();
+
+app.MapControllers();
+
+app.Run();
